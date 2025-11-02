e6f59b36d0ef4cc3f639fde75ee40c2fc811d50e
DC2828 <mvkin2014@gmail.com>
2025-11-02 21:48:28 +0800

    Signed Server Error

--- server.ts
+++ server.ts
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import { initDatabase } from './models/backend/init/init.db';
 import ConfigManager from './models/config/config.manager';
 import backendRouter from './models/backend/backend.router';
+import { pageRouter } from './models/frontend/page/router';
+import path from 'path';
 
 const app = express();
 // Security & performance middleware
@@ -14,19 +16,32 @@ app.use(compression());
 app.use(morgan('combined'));
 
 // Core parsers
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+//set static folder
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname,'/src')));
 
 // Config manager initialization
 const Config: ConfigManager = ConfigManager.getConfigManager();
 
 // Mount model-based routers
 app.use('/api/:api_version/', backendRouter);
-
+app.use('/page',pageRouter);
 // Health
 // app.get('/health', (_req: Request, res: Response) => {
 // 	res.json({ ok: true });
 // });
+
+app.get('/', (_req: Request, res: Response) => {
+	res.redirect('/page/credential');
+});
 
 // Basic error handler, returns 500 if unhandled
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
